2b594dadb7afb61b3c07299b7a840c74a4aed30c
Sean Ward <seanhward@gmail.com>
2022-12-16 14:14:44 -0800

    initial

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,7 +7,7 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: "text-davinci-003",
     prompt: generatePrompt(req.body.animal),
     temperature: 0.6,
   });
@@ -17,12 +17,15 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+  return (
+    `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+    Animal: Cat
+    Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+    Animal: Dog
+    Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+    Animal: ${capitalizedAnimal}
+    Names:`
+  );
 }
+
